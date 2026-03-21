06e0577986da651b01d6a8377fe619b8ff81b633
Akio Ito <akio.ito@carenet.co.jp>
2026-03-22 06:44:58 +0900

    adjust neovide_scroll_animation_length

--- vim/.config/nvim/lua/configs.lua
+++ vim/.config/nvim/lua/configs.lua
@@ -43,7 +43,7 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animate_in_insert_mode = false
   vim.g.neovide_cursor_animate_command_line = false
   vim.g.neovide_scroll_animation_far_lines = 0
-  vim.g.neovide_scroll_animation_length = 0.00
+  vim.g.neovide_scroll_animation_length = 0.3
 end
 
 -- Cursor blink (blinkwait-blinkon-blinkoff in milliseconds)
